1aec6975c1e73206473cebb9542613ae1199606e
Pk Bhai <pb6620113@gmail.com>
2025-10-21 13:33:07 +0000

    18:55:54.269 Running build in Washington, D.C., USA (East) – iad1 18:55:

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // Vercel environment might require logging to be less verbose
-    // log: ["query", "info", "warn", "error"],
-  });
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
